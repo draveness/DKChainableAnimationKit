75aa805b6bf4eb64bdbbeb66f30bc5c1e0df6ee1
Draveness <stark.draven@gmail.com>
2015-05-14 12:13:19 +0800

    Translate c to swift

--- Classes/NSBKeyframeAnimationFunctionLinear.swift
+++ Classes/NSBKeyframeAnimationFunctionLinear.swift
@@ -260,11 +260,14 @@ func NSBKeyframeAnimationFunctionEaseOutBounce(var t: Double, b: Double, c: Doub
     if (t < (1/2.75)) {
         return c*(7.5625*t*t) + b;
     } else if (t < (2/2.75)) {
-        return c*(7.5625*(t-=(1.5/2.75))*t + 0.75) + b;
+        t-=(1.5/2.75)
+        return c*(7.5625*t*t + 0.75) + b;
     } else if (t < (2.5/2.75)) {
-        return c*(7.5625*(t-=(2.25/2.75))*t + 0.9375) + b;
+        t-=(2.25/2.75)
+        return c*(7.5625*t*t + 0.9375) + b;
     } else {
-        return c*(7.5625*(t-=(2.625/2.75))*t + 0.984375) + b;
+        t-=(2.625/2.75)
+        return c*(7.5625*t*t + 0.984375) + b;
     }
 }
 
