7acb381b13a226fd3c978f6049a138cd3ca8359b
Draveness <stark.draven@gmail.com>
2015-05-22 10:31:44 +0800

    Add target

---
+++ DKChainableAnimationKit/DKChainableAnimationKit.h
@@ -0,0 +1,19 @@
+//
+//  DKChainableAnimationKit.h
+//  DKChainableAnimationKit
+//
+//  Created by apple on 15/5/22.
+//  Copyright (c) 2015年 DeltaX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DKChainableAnimationKit.
+FOUNDATION_EXPORT double DKChainableAnimationKitVersionNumber;
+
+//! Project version string for DKChainableAnimationKit.
+FOUNDATION_EXPORT const unsigned char DKChainableAnimationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKChainableAnimationKit/PublicHeader.h>
+
+
---
+++ DKChainableKit/DKChainableKit.h
@@ -0,0 +1,19 @@
+//
+//  DKChainableKit.h
+//  DKChainableKit
+//
+//  Created by apple on 15/5/22.
+//  Copyright (c) 2015年 DeltaX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DKChainableKit.
+FOUNDATION_EXPORT double DKChainableKitVersionNumber;
+
+//! Project version string for DKChainableKit.
+FOUNDATION_EXPORT const unsigned char DKChainableKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKChainableKit/PublicHeader.h>
+
+
